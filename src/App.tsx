cf849ece6fda4116cedfd0be739ceebaa332e2c4
Junya <61687637+newJuniorV@users.noreply.github.com>
2022-09-05 17:11:03 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useState } from "react";
+import "./styles.css";
+import { Todo } from "./Todo";
+
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+export default function App() {
+  const [todos, setTodos] = useState<any>([]);
+
+  const onClickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      setTodos(res.data);
+    });
+  };
+  return (
+    <div className="App">
+      <button onClick={onClickFetchData}>get data</button>
+      {todos.map((todo) => (
+        <Todo userid={todo.userid} title={todo.title} />
+      ))}
+    </div>
+  );
+}
